ede91facd8ed329aa5021ce53425391670dc4a7e
Mattias-08 Jörntell <mattias.jorntell@gmail.com>
2024-07-14 17:15:00 +0000

    renaming files to make the server work

--- booking_system/settings.py
+++ booking_system/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-uli-l-$qlu74k09h2lswu0x)%*4v8nj(h^ilq$gi(cce5hsne1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ '8000-mattias08-restaurantboo-y7l3bovjsjp.ws-eu115.gitpod.io']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'reservations',
 ]
 
 MIDDLEWARE = [
