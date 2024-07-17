c022852c1723035d22ceaeb97cc546172a15c3f2
Mattias-08 Jörntell <mattias.jorntell@gmail.com>
2024-07-17 19:54:27 +0000

    add secret key

--- booking_system/settings.py
+++ booking_system/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-uli-l-$qlu74k09h2lswu0x)%*4v8nj(h^ilq$gi(cce5hsne1'
+SECRET_KEY = os.environ.get("reservations")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
