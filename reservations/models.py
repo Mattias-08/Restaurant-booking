498fbe2ab65f4e655abe31f9aebe03c7dc4223bd
Mattias-08 Jörntell <mattias.jorntell@gmail.com>
2024-07-20 14:14:03 +0000

    removing models

--- reservations/models.py
+++ reservations/models.py
@@ -1,23 +1,5 @@
 from django.db import models
 
-# Create your models here.
-class User(models.Model):
-    username = models.CharField(max_length=255, unique=True)
-    email = models.EmailField(unique=True)
-    password = models.CharField(max_length=128)  # Replace with a suitable password hashing method
-    phone_number = models.CharField(max_length=20, blank=True)
-
-    def __str__(self):
-        return self.username
-
-class Table(models.Model):
-    amount_of_seats = models.PositiveIntegerField()
-    table_num = models.CharField(max_length=10, unique=True)
-    require_reservations = models.BooleanField(default=True)
-
-    def __str__(self):
-        return f"Table #{self.table_num} - Seats: {self.amount_of_seats}"
-
 class Reservation(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     table_id = models.ForeignKey(Table, on_delete=models.CASCADE)
