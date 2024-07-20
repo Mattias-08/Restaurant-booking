636dfaa1e7300c74ecf2fe703ee557c0bc68a3c2
Mattias-08 Jörntell <mattias.jorntell@gmail.com>
2024-07-20 15:20:21 +0000

    add post model

--- reservations/models.py
+++ reservations/models.py
@@ -1,12 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Reservation(models.Model):
     customer_full_name = models.CharField(max_length=255)
     # Comma-separated list of table numbers (if applicable for many tables)
-    tables = models.CharField(max_length=255, blank=True, help_text="Comma-separated list of table numbers")
+    tables = models.CharField(max_length=255, blank=True)
     date = models.DateField()
     time = models.TimeField()
-    customer_full_name = models.CharField(max_length=255)
 
     def __str__(self):
         return f"Reservation ID: {self.id} - {self.user_id} ({self.date} - {self.time})"
