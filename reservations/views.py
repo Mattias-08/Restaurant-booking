d26fc70a3aa91132974582a415d168e4fb4a7b7f
Mattias Jörntell <mattias.jorntell@gmail.com>
2024-08-03 14:39:58 +0000

    add forms.py and adjust views.py

--- reservations/views.py
+++ reservations/views.py
@@ -1,8 +1,29 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.views.generic import ListView
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from .models import Reservation
+from .forms import ReservationForm 
 
-class MyListView(ListView):
-    model = Reservation
-    template_name = "reservations/index.html"
+
+def home(request):
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            form = ReservationForm(request.POST)
+            if form.is_valid():
+                reservation = form.save(commit=False)
+                reservation.user = request.user 
+  # Assuming a User field in Reservation model
+                if reservation.is_table_available():
+                    reservation.save()
+                    messages.success(request, 'Reservation created successfully!')
+                else:
+                    messages.error(request, 'Table not available.')
+            else:
+                messages.error(request, 'Invalid form data.')
+        else:
+            form = ReservationForm()
+
+        user_reservations = Reservation.objects.filter(user=request.user)
+        context = {'form': form, 'user_reservations': user_reservations}
+        return render(request, 'home.html', context)
+    else:
+        return render(request, 'home.html')
