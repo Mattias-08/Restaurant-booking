521396b5b27ceedd074be52c55baa2e2d3b0ce10
Mattias Jörntell <mattias.jorntell@gmail.com>
2024-08-04 10:36:35 +0000

    adjust indention

--- reservations/models.py
+++ reservations/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from django.core.exceptions import ValidationError
 from django.utils.text import slugify
 from django.core.validators import MinValueValidator
-from django.utils import timezone 
+from django.utils import timezone
 
 
 STATUS = ((0, "Draft"), (1, "Published"))
@@ -19,7 +19,7 @@ TIME_PERIODS = (
 )
 # Table number, name, number of seats
 TABLES = (
-    (1, 'Table 1', 2), 
+    (1, 'Table 1', 2),
     (2, 'Table 2', 2),
     (3, 'Table 3', 4),
     (4, 'Table 4', 4),
@@ -33,8 +33,8 @@ TABLES = (
     (12, 'Table 12', 4),
     (13, 'Table 13', 4),
     (14, 'Table 14', 4),
-    (15, 'Table 15', 8),  
-    (16, 'Table 16', 8),  
+    (15, 'Table 15', 8),
+    (16, 'Table 16', 8),
 )
 
 class Reservation(models.Model):
@@ -44,24 +44,17 @@ class Reservation(models.Model):
   table_number = models.IntegerField(choices=TABLES, default=1),
 
   
-
   def validate_date(value):
     """
     Custom validation function to check the reservation date.
 
     This function ensures the following:
-      - Reservations cannot be made for today or past dates (minimum allowed date is tomorrow).
-      - You can add additional validation logic here, such as checking for specific date ranges based on a `max_booking_date` field (not implemented here).
-
-    Args:
-      value: The date value to be validated.
-
+      - Reservations cannot be made for today or past dates
     Raises:
       ValidationError: If the date is today or in the past.
     """
     if value <= timezone.now().date():
-      raise ValidationError('Reservations cannot be made for today or past dates.')
-    # Add additional validation logic here (e.g., check for specific date ranges)
+      raise ValidationError('Reservations cannot only be made for future dates')
 
   date = models.DateField(validators=[validate_date])
 
@@ -80,16 +73,20 @@ class Reservation(models.Model):
 
   def save(self, *args, **kwargs):
     if not self.is_table_available():
-      raise ValidationError('Selected table is not available for this time slot.')
+      raise ValidationError + \
+        ('Selected table is not available for this time slot.')
 
-    # Generate slug using slugify with allow_unicode=True for potential internationalization
+    # Generate slug using slugify with allow_unicode=True
+    # for potential internationalization
     self.slug = slugify(self.name, allow_unicode=True)
 
-    # Call the original save method to persist the reservation after successful validation
+    # Call the original save method to persist the reservation after validation
     super().save(*args, **kwargs)
 
   def __str__(self):
     selected_time = TIME_PERIODS[self.time_slot][1]
     table_info = [t for t in TABLE_CHOICES if t[0] == self.table_number][0]
-    table_name = table_info[1] if table_info[1] else f"Table {self.table_number}"  # Use table name if provided
-    return f"Reservation ID: {self.id} - {self.customer_full_name} ({self.date} - {selected_time}, Table: {table_name})"
+    table_name = + \
+      table_info[1] if table_info[1]  else f"Table {self.table_number}"
+    return f"Reservation ID: {self.id} - {self.customer_full_name} + \
+       ({self.date} - {selected_time}, Table: {table_name})"
--- reservations/views.py
+++ reservations/views.py
@@ -34,14 +34,16 @@ def make_reservation(request):
   # Don't save immediately for validation
             if reservation.is_table_available():  # Check for table availability
                 reservation.save()
-                messages.success(request, 'Your reservation has been submitted successfully! We will confirm your reservation shortly.')
+                messages.success(request, '''Your reservation has 
+                    been submitted successfully! 
+                    We will confirm your reservation shortly.''')
                 return redirect('home')  # Redirect to home page after success
             else:
-                form.add_error(None, 'Selected table is not available for this time slot.')
+                form.add_error(None, + \
+                    'Selected table is not available for this time slot.')
     else:
         form = ReservationForm()
     return render(request, 'base.html', {'form': form})
-
 
 def custom_404(request, exception=None):
     return render(request, '404.html', status=404)
