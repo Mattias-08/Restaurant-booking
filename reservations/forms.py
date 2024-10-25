8bbf307c2ae8d289398154e8a31b0d2de53fc3ca
Mattias Jörntell <mattias.jorntell@gmail.com>
2024-10-25 18:00:23 +0000

    update forms.py

--- reservations/forms.py
+++ reservations/forms.py
@@ -4,4 +4,4 @@ from .models import Reservation
 class ReservationForm(forms.ModelForm):
     class Meta:
         model = Reservation
-        fields = ['customer_full_name', 'date', 'time_slot']
+        fields = ['date', 'time_slot', 'table']
