752880cc7d94b1d61b436d2c83c7fcc648d1b918
Mattias Jörntell <mattias.jorntell@gmail.com>
2024-11-05 08:14:19 +0000

    add comments to forms

--- reservations/forms.py
+++ reservations/forms.py
@@ -2,6 +2,22 @@ from django import forms
 from .models import Reservation
 
 class ReservationForm(forms.ModelForm):
+    """
+    A form for creating and updating reservations.
+
+    **Fields**
+    
+    ``date``
+        The date of the reservation, displayed as a date input field.
+    ``time_slot``
+        The time slot of the reservation, displayed as a select dropdown.
+    ``table``
+        The table for the reservation, displayed as a select dropdown.
+
+    **Meta Class**
+    
+    Defines the model, fields, and widgets used in the form.
+    """
     class Meta:
         model = Reservation
         fields = ['date', 'time_slot', 'table']
@@ -12,5 +28,9 @@ class ReservationForm(forms.ModelForm):
         }
 
     def __init__(self, *args, **kwargs):
+        """
+        Initialize the form and set the date input format.
+
+        """
         super(ReservationForm, self).__init__(*args, **kwargs)
         self.fields['date'].input_formats = ['%Y-%m-%d']
